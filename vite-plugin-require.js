33b89e71130bd5a43ff971f8807957e0814f7262
kazisolah114 <kazisolayman123@gmail.com>
2024-04-14 16:23:13 +0600

    attempt to fixed t-creation bug

--- vite-plugin-require.js
+++ vite-plugin-require.js
@@ -1,12 +1,12 @@
-export default function VitePluginRequire() {
-    return {
-      name: 'vite-plugin-require',
-      transform(code) {
-        return {
-          code: code.replace(/require\(['"](.+?)['"]\)/g, "import('$1')"),
-          map: null,
-        };
-      },
-    };
-  }
+// export default function VitePluginRequire() {
+//     return {
+//       name: 'vite-plugin-require',
+//       transform(code) {
+//         return {
+//           code: code.replace(/require\(['"](.+?)['"]\)/g, "import('$1')"),
+//           map: null,
+//         };
+//       },
+//     };
+//   }
   
