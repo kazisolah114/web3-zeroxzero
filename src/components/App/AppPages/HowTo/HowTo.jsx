fa3792e977ac2d36bd2e9c032312e3c8783fefb9
kazisolah114 <kazisolayman123@gmail.com>
2024-04-17 11:23:44 +0600

    radnom

--- src/components/App/AppPages/HowTo/HowTo.jsx
+++ src/components/App/AppPages/HowTo/HowTo.jsx
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import './HowTo.css'
 import FAQ from './FAQ';
-import { Link } from 'lucide-react';
 
 const HowTo = () => {
     const [activeTab, setActiveTab] = useState("trade");
